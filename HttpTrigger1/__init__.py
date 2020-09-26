d55e2da645087cc4cfdb49bee8e34c465ff8a308
hemanth <hemanth.kotagiri@xdinnovations.org>
2020-09-26 19:59:06 +0530

    Complete TASK #01 - Covert data from database to csv with a http trigger function

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,24 +1,48 @@
 import logging
-
+import json, os, csv, pymongo, urllib
+from pymongo import MongoClient
 import azure.functions as func
+
+def convert_data_to_csv():
+
+    username = urllib.parse.quote_plus(os.environ.get("MONGO_USERNAME"))
+    password = urllib.parse.quote_plus(os.environ.get("MONGO_PASSWORD"))
+
+    print(username, password)
+    url = f'mongodb+srv://{username}:{password}@cluster0-practice.tmxrp.azure.mongodb.net/practice-database?retryWrites=true&w=majority'
+
+    cluster = MongoClient(url)
+
+    db = cluster["practice-database"]
+    student = db["student"]
+
+    data = list(student.find({})) # Wrapping the cursor object(possibly a generator) to a list.
+
+    with open("data.csv", "w") as new_csv_file:
+        csv_writer = csv.writer(new_csv_file)
+        csv_writer.writerow(data[0].keys()) # Writing the first row to be the fields.
+        for data_point in data:
+            print(data_point)
+            csv_writer.writerow(data_point.values())
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    convert = req.params.get('convert')
+    if not convert:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            convert = req_body.get('convert')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if convert:
+        convert_data_to_csv()
+        return func.HttpResponse(f"The student data from the database has been convered to csv succesfully!")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "The function triggered successfully. Pass a convert=true value in the query string or in the request body for converting the data in database to csv.",
              status_code=200
         )
--- populate_database_script.py
+++ populate_database_script.py
@@ -16,15 +16,16 @@ student = db["student"]
 
 # Populating the database with 150 hypothetical student details
 
-names = json.loads(open("names.json", "r").read())
+def populate_database(collection):
+    names = json.loads(open("names.json", "r").read())
 
-for i,name in enumerate(names):
-    post = {
-        "_id": i,
-        "first_name": name,
-        "roll_num": i+1,
-        "email": ''.join([name,"@school.com"]).lower(),
-        "class": random.choice([i for i in range(7, 13)]),
-        "section": random.choice(["A", "B"])
-    }
-    student.insert_one(post)
+    for i,name in enumerate(names):
+        post = {
+            "_id": i,
+            "first_name": name,
+            "roll_num": i+1,
+            "email": ''.join([name,"@school.com"]).lower(),
+            "class": random.choice([i for i in range(7, 13)]),
+            "section": random.choice(["A", "B"])
+        }
+        collection.insert_one(post)
